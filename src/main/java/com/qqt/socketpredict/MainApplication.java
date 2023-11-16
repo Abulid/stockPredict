7c3630ce52d5dae517fb45b89e1eb6030b5434fb
qqt <1505576921@qq.com>
2023-11-16 21:52:30 +0800

    Initial commit

---
+++ src/main/java/com/qqt/socketpredict/MainApplication.java
@@ -0,0 +1,13 @@
+package com.qqt.socketpredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketPredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketPredictApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qqt/socketpredict/SocketPredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqt.socketpredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketPredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
